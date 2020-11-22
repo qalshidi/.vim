04b0527324a93d293ae281c04688f06f922a0802
Qusai Al Shidi <me@qalshidi.science>
2020-11-22 17:43:40 -0500

    MAINT: Neovim breaking changes update.

--- lua/lsp-config.lua
+++ lua/lsp-config.lua
@@ -1,47 +1,44 @@
-
-local nvim_lsp = require("nvim_lsp")
+local lspconf = require("lspconfig")
 local nvim_completion = require("completion")
-local nvim_diagnostic = require("diagnostic")
 
 local custom_attach = function()
     nvim_completion.on_attach()
-    nvim_diagnostic.on_attach()
     print("LSP Attached.")
 end
 
-if not nvim_lsp.vimls.install_info().is_installed then
-    nvim_lsp.vimls.install()
+if not lspconf.vimls.install_info().is_installed then
+    lspconf.vimls.install()
 end
-nvim_lsp.vimls.setup{ on_attach = custom_attach }
+lspconf.vimls.setup{ on_attach = custom_attach }
 
-if not nvim_lsp.bashls.install_info().is_installed then
-    nvim_lsp.bashls.install()
+if not lspconf.bashls.install_info().is_installed then
+    lspconf.bashls.install()
 end
-nvim_lsp.bashls.setup{ on_attach = custom_attach }
+lspconf.bashls.setup{ on_attach = custom_attach }
 
 if vim.fn.executable("haskell-language-server-wrapper") then
-    nvim_lsp.hls.setup{ on_attach = custom_attach }
+    lspconf.hls.setup{ on_attach = custom_attach }
 else
     vim.fn.jobstart("ghcup upgrade && ghcup install hls")
 end
 
 if vim.fn.executable("pyls") then
-    nvim_lsp.pyls.setup{ on_attach = custom_attach }
+    lspconf.pyls.setup{ on_attach = custom_attach }
 else
     vim.fn.jobstart("python3 -m pip install --user -U --force python-language-server")
 end
 
 if vim.fn.executable("texlab") then
-    nvim_lsp.texlab.setup{ on_attach = custom_attach }
+    lspconf.texlab.setup{ on_attach = custom_attach }
 else
     vim.fn.jobstart("cargo install texlab")
 end
 
 if vim.fn.executable("clangd") then
-    nvim_lsp.clangd.setup{ on_attach = custom_attach }
+    lspconf.clangd.setup{ on_attach = custom_attach }
 end
--- nvim_lsp.sumneko_lua.setup {
--- cmd = { vim.fn.stdpath("cache") .. "/nvim_lsp/sumneko_lua/lua-language-server/bin/Linux/lua-language-server", "-E", vim.fn.stdpath("cache") .. "/nvim_lsp/sumneko_lua/lua-language-server/main.lua" },
+-- lspconf.sumneko_lua.setup {
+-- cmd = { vim.fn.stdpath("cache") .. "/lspconf/sumneko_lua/lua-language-server/bin/Linux/lua-language-server", "-E", vim.fn.stdpath("cache") .. "/lspconf/sumneko_lua/lua-language-server/main.lua" },
 -- on_attach = custom_attach,
 -- settings = {
 --   Lua = {
@@ -52,5 +49,5 @@ end
 -- },
 -- }
 
--- nvim_lsp.rust_analyzer.setup{ on_attach = custom_attach }
+-- lspconf.rust_analyzer.setup{ on_attach = custom_attach }
 
