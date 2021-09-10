ad80258e45c5f7f2567a7d3f430dcfa7dcc5ff2b
Qusai Al Shidi <me@qalshidi.science>
2021-09-10 11:25:47 -0400

    Fri 10 Sep 2021 11:25:47 AM EDT

--- lua/lsp-config.lua
+++ lua/lsp-config.lua
@@ -1,17 +1,29 @@
 local lspconf = require("lspconfig")
 local nvim_completion = require("completion")
 
-local custom_attach = function()
+local custom_attach = function(client, bufnr)
     nvim_completion.on_attach()
     print("LSP Attached.")
 end
 
-lspconf.vimls.setup{ on_attach = custom_attach }
-lspconf.bashls.setup{ on_attach = custom_attach }
-lspconf.hls.setup{ on_attach = custom_attach }
-lspconf.pyright.setup{ on_attach = custom_attach }
-lspconf.texlab.setup{ on_attach = custom_attach }
-lspconf.clangd.setup{ on_attach = custom_attach }
+local servers = {
+    "vimls",
+    "bashls",
+    "hls",
+    "pyright",
+    "texlab",
+    "clangd",
+}
+
+for _, server in ipairs(servers) do
+    lspconf[server].setup {
+        on_attach = custom_attach,
+        flags = {
+            debounce_text_changes = 150,
+        }
+    }
+end
+
 -- lspconf.sumneko_lua.setup {
 -- cmd = { vim.fn.stdpath("cache") .. "/lspconf/sumneko_lua/lua-language-server/bin/Linux/lua-language-server", "-E", vim.fn.stdpath("cache") .. "/lspconf/sumneko_lua/lua-language-server/main.lua" },
 -- on_attach = custom_attach,
